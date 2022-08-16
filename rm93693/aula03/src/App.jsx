f22fa23602e8ce2dff008fa82894f3b406683f2f
phalves23 <rm93693@fiap.com.br>
2022-08-16 12:18:37 -0300

    :sparkles: aprendendo a puxar informações de uma API

--- rm93693/aula03/src/App.jsx
+++ rm93693/aula03/src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ApiExterna from "./components/apiexterna/ApiExterna";
+import Calculo from "./components/Calculo";
 import Componente1 from "./components/componente1/Componente1";
 
 export default () => {
@@ -8,6 +10,8 @@ export default () => {
       <div>
         <h2>Ex: de PROPS-CHILDREN</h2>
         <Componente1 />
+        <Calculo />
+        <ApiExterna />
       </div>
     </>
   );
