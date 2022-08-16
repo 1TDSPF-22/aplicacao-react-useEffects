0f0d60cc40d3b109e91ea309eaa1d68e4e969994
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-16 08:51:36 -0300

    Exemplos de props.children

---
+++ pf0670/aula03/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Componente1 from "./components/componente1/Componente1"
+
+export default function App() {
+    return (
+        <div>
+            <h1>Estudo sobre useEffects e props.children</h1>
+            <div>
+                <h2>Ex: de PROPS-CHILDREN</h2>
+                <Componente1 />
+            </div>
+        </div>
+    )
+}
