3334c764fa50b1726956b6100b840d897b4ff7b2
YasminCabralDias <rm95800@fiap.com.br>
2022-08-19 11:27:21 -0300

    .

--- rm95800/aula03/src/App.jsx
+++ rm95800/aula03/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react"
 import ApiExterna from "./components/apiExterna/ApiExterna"
 import Calculo from "./components/calculo/Calculo"
 import Componente1 from "./components/componente1/Componente1"
-
+import Relogio from "./components/relogio/Relogio"
+import GitUsers from "./components/gitusers/GitUsers"
 export default function App() {
     return (
         <div>
@@ -12,6 +13,7 @@ export default function App() {
                 <Componente1 /> 
                 <h2>Ex: de renderização condicional!</h2>
                 <Calculo />*/}
+                <Relogio/>
                 <ApiExterna />
             </div>
         </div>
---
+++ rm95800/aula03/src/components/relogio/Relogio.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react"
+
+
+export default function Relogio (){
+
+    const [relogio, setRelogio] = useState(new Date().toLocaleTimeString())
+
+    setTimeout(()=> {
+       setRelogio(new Date ().toLocaleTimeString())
+    }, 1000);
+
+    return(
+        <div>
+            <h2>Time: {relogio}</h2>
+        </div>
+    )
+}
