f030a1f87bda1452b22c06a745d08129e107506b
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-19 12:24:26 -0300

    Exercício

--- pf0670/aula03/src/components/relogio/Relogio.jsx
+++ pf0670/aula03/src/components/relogio/Relogio.jsx
@@ -7,6 +7,7 @@ export default function Relogio(){
     const [relogio, setRelogio] = useState(hora)
 
     setTimeout(() => {
+        hora = new Date().toLocaleTimeString()
        setRelogio(hora)         
     }, 1000);
 
