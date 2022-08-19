e188f920325d6d6b51b1b9b90010e41180515777
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-19 11:13:46 -0300

    :sparkles: desenvolvendo componente Relogio

--- rm93693/aula03/src/App.jsx
+++ rm93693/aula03/src/App.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ApiExterna from "./components/apiexterna/ApiExterna";
 import Calculo from "./components/Calculo";
 import Componente1 from "./components/componente1/Componente1";
+import Relogio from "./components/relogio/Relogio";
 
-export default () => {
+export default function App() {
   return (
     <>
       <h1>Estudo sobre useEffects e props.children</h1>
@@ -11,6 +12,7 @@ export default () => {
         <h2>Ex: de PROPS-CHILDREN</h2>
         <Componente1 />
         <Calculo />
+        <Relogio/>
         <ApiExterna />
       </div>
     </>
---
+++ rm93693/aula03/src/components/relogio/Relogio.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react"
+export default function Relogio(){
+    let hora = new Date().toLocaleTimeString()
+    const [relogio, setRelogio] = useState(hora)
+    setTimeout(() => {
+        setRelogio(hora)
+    },1000)
+
+
+    return(
+        <div>
+            <h2>Time: {relogio} </h2>
+        </div>
+    )
+}
