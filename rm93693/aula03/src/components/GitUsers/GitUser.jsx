5926f6f749d9f0cb65ec4c5c3acdc47ce3daf0f9
phalves23 <rm93693@fiap.com.br>
2022-08-20 11:27:54 -0300

    :lipstick: desenvolvendo componente card do GitUser

--- rm93693/aula03/src/components/GitUsers/GitUser.jsx
+++ rm93693/aula03/src/components/GitUsers/GitUser.jsx
@@ -1,36 +1,43 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
+import GitHubBG from "../../assets/img/github-bg.svg";
+import "./GitUser.css";
 
-export default function GitUser(){
-    // const objeto = {
-    //     nome: "Duda",
-    //     idade: 18,
-    //     profissao: "Universitária",
-    //     rm: 95854
-    // }
+export default function GitUser() {
+  // const objeto = {
+  //     nome: "Duda",
+  //     idade: 18,
+  //     profissao: "Universitária",
+  //     rm: 95854
+  // }
 
-    const[listaUsuarios, setListaUsuarios] = useState([]);
+  const [listaUsuarios, setListaUsuarios] = useState([]);
 
-    const carregaLista = async ()=>{
-        const response = await fetch("https://api.github.com/users");
-        const data = await response.json()
-        setListaUsuarios(data)
-    }
+  const carregaLista = async () => {
+    const response = await fetch("https://api.github.com/users");
+    const data = await response.json();
+    setListaUsuarios(data);
+  };
 
-
-    return(
-        <>
-            <div>
-                <h1>GitUsers</h1>
-                <ul>
-                    {listaUsuarios.map((usuario)=>
-                    <li key={usuario.id}>{usuario.login}</li>
-                    )}
-                </ul>
+  return (
+    <>
+      <div>
+        <h1>Card de usuário</h1>
+        <ul>
+          {listaUsuarios.map((usuario) => (
+            <div key={usuario.id} className="gituser-card">
+              <div className="gituser-img">
+                <img src={usuario.avatar_url} alt="avatar" />
+              </div>
+              <h1>{usuario.login}</h1>
+              <a href={usuario.html_url}>{usuario.html_url}</a>
             </div>
+          ))}
+        </ul>
+      </div>
 
-            <button onClick={()=> carregaLista()}>Carregar Usuários</button>
-        </>
-    )
+      <button onClick={() => carregaLista()}>Carregar Usuários</button>
+    </>
+  );
 }
 //Exercicio
 /* 
